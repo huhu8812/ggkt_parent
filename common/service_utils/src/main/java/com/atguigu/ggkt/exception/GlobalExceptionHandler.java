6ef08bc5900e74bab03bbc0817a453d8f60cd46c
humingyuan <mingyuan.hu@thoughtworks.com>
2022-08-22 00:10:51 +0800

    # 添加全局异常处理器

---
+++ common/service_utils/src/main/java/com/atguigu/ggkt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.atguigu.ggkt.exception;
+
+import com.atguigu.ggkt.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理类
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail(null).message("执行了全局异常处理");
+    }
+}
